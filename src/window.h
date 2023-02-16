be691ebb04f5c0f442bc3cd98df1eaf4d3ea00f2
Lars Magnus Nordeide <600871@stud.hvl.no>
2023-02-16 12:25:31 +0100

    callback render and update

--- src/window.h
+++ src/window.h
@@ -1,15 +1,29 @@
 #include <windows.h>
 #include <iostream>
+#include <functional>
+#include <map>
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 class Window {
 public:
+    static Window* instance;
+public:
     Window();
     ~Window();
+
+    void start();
+    void render();
+    void update();
+
+    void setRenderCallback(std::function<void()> render);
+    void setUpdateCallback(std::function<void()> update);
 
     bool ProcessMessages();
 private:
     HINSTANCE m_hInstance;
     HWND m_hWnd;
+
+    std::function<void()> m_hRender = [](){};
+    std::function<void()> m_hUpdate = [](){};
 };
