fbc83b436e9ff8000a87e7de0cfcd269e6227238
Lars Magnus Nordeide <600871@stud.hvl.no>
2023-02-16 12:23:54 +0100

    cleanup

--- src/Program.cpp
+++ src/Program.cpp
@@ -1,5 +1,3 @@
-#pragma once
-
 #include "Program.h"
 #include <iostream>
 
--- src/window.h
+++ src/window.h
@@ -1,11 +1,7 @@
 #include <windows.h>
 #include <iostream>
 
-//ATOM                MyRegisterClass(HINSTANCE hInstance);
-//BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-//INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-//INT_PTR CALLBACK    MyDlg(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 class Window {
 public:
